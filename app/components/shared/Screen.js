68df350bba1054c2e81d1a82beb80821151987dd
Marcus Christiansson <christiansson.marcus@gmail.com>
2022-05-22 16:45:54 +0200

    Update Screen.js

--- app/components/shared/Screen.js
+++ app/components/shared/Screen.js
@@ -1,22 +1,46 @@
 import { useScreen } from "@ckbab/react-native-components/hooks";
 import { shadow2 } from "@ckbab/react-native-components/styles";
 import PropTypes from "prop-types";
-import React, { useLayoutEffect, useState } from "react";
+import React, { useEffect, useLayoutEffect, useState } from "react";
+import { Keyboard, Platform } from "react-native";
 import { backgroundColor, primaryColor } from "../../constants/colors";
 import ToolbarButton from "./ToolbarButton";
 
 export default function Screen({ children, navigation }) {
   const { bottomMargin } = useScreen();
   const [isScrolled, setIsScrolled] = useState(false);
+  const [keyboardHeight, setKeyboardHeight] = useState(0);
+
+  useEffect(() => {
+    const keyboardShow = (event) => {
+      setKeyboardHeight(event.endCoordinates.height);
+    };
+
+    const keyboardHide = () => {
+      setKeyboardHeight(0);
+    };
+
+    const willShow = Keyboard.addListener("keyboardWillShow", keyboardShow);
+    const willHide = Keyboard.addListener("keyboardWillHide", keyboardHide);
+    const didShow = Keyboard.addListener("keyboardDidShow", keyboardShow);
+    const didHide = Keyboard.addListener("keyboardDidHide", keyboardHide);
+
+    return () => {
+      willShow?.remove();
+      willHide?.remove();
+      didShow?.remove();
+      didHide?.remove();
+    };
+  }, []);
 
   useLayoutEffect(() => {
     const shadow = isScrolled ? shadow2 : null;
     navigation.setOptions({
       headerLeft: () => (
-        <ToolbarButton icon="back" onPress={navigation.goBack} />
+        <ToolbarButton icon="chevron-back" onPress={navigation.goBack} />
       ),
       headerTitleStyle: {
-        fontFamily: "Roboto-Bold",
+        fontFamily: "Arsenal-Bold",
         fontSize: 20,
         color: primaryColor,
       },
@@ -36,8 +60,14 @@ export default function Screen({ children, navigation }) {
     }
   };
 
+  // Note that on Android keyboard is not overlapping content.
+  const paddingBottom = Platform.select({
+    android: 0,
+    ios: keyboardHeight > 0 ? keyboardHeight : bottomMargin,
+  });
+
   return children
-    ? children({ backgroundColor, paddingBottom: bottomMargin, onScroll })
+    ? children({ backgroundColor, paddingBottom, onScroll })
     : null;
 }
 
